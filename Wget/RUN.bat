6a213d296c098ba4abb4f5fa4e6ac7c2821b34d7
pmathapa <pmathapa@MATHWORKS-Wr4wp.dhcp.mathworks.com>
2024-09-09 16:33:23 +0530

    batch added

---
+++ Wget/RUN.bat
@@ -0,0 +1,21 @@
+set PATH=C:\Polyspace\PolyspaceDesktop\R2024a\polyspace\bin;%PATH%
+
+set ps_helper_access=polyspace-access -host demo-polyspace-review.gnb.mathworks.com -port 9443 -protocol http -credentials-file c:\Polyspace\PolyspaceServer\myCredentials.txt
+set RESULT=ResultBF
+set PROG=WgetCI
+set PARENT_PROJECT_ON_ACCESS=public/pmathapa
+set WSP=%WORKSPACE%
+rd /S /Q Notification && md Notification
+
+
+polyspace-bug-finder-server.exe -options-file "%WSP%\target.opts" -options-file "%WSP%\PSOptions.opts" -results-dir "%WSP%\R_BF_%BUILD_NUMBER%"   ||  EXIT /B 200
+
+%ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
+
+%ps_helper_access% -upload "%WSP%\R_BF_%BUILD_NUMBER%" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%_%BUILD_NUMBER%"	||  EXIT /B 200
+
+REM %ps_helper_access% -export %PARENT_PROJECT_ON_ACCESS%/%PROG%_%BUILD_NUMBER% -output Results_All.tsv -defects High    ||  EXIT /B 200
+
+exit 0
+ 
+
